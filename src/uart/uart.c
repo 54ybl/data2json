327c037ffe829df4109f7e3d84e78504970aa314
54ybl <594ybl@gmail.com>
2023-05-13 10:36:40 +0800

    提交仓库文件

---
+++ src/uart/uart.c
@@ -0,0 +1,28 @@
+/*
+ * uart.c
+ *
+ * created: 2023/5/11
+ *  author: 
+ */
+
+
+
+#include "uart.h"
+#include "ls1b.h"
+#include "ls1b_gpio.h"
+#include "ns16550.h"
+#include "stdio.h"
+#include "string.h"
+#include "uart.h"
+
+void UART5_Config_Init(void)
+{
+    unsigned int BaudRate = 115200;
+    ls1x_uart_init(devUART5,(void *)BaudRate); //初始化串口
+    ls1x_uart_open(devUART5,NULL); //打开串口
+}
+
+void UART5_Test(char* buff){
+    //发送数据
+    ls1x_uart_write(devUART5,buff,strlen(buff),NULL);
+}
---
+++ src/uart/uart.h
@@ -0,0 +1,23 @@
+/*
+ * uart.h
+ *
+ * created: 2023/5/11
+ *  author: 
+ */
+
+#ifndef _UART_H
+#define _UART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void UART5_Config_Init(void);
+void UART5_Test(char* buff);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _UART_H
+
